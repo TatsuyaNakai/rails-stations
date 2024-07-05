4cbd8b9fd1670bad4447eaf109fb5093fd74957c
tatsuya.nakai <15g079nt@gmail.com>
2024-07-05 13:27:16 +0900

    part6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,18 @@ class MoviesController < ApplicationController
   # GET /movies or /movies.json
   def index
     @movies = Movie.all
+
+    if params[:keyword].present?
+      @movies = @movies.merge(
+        Movie.where('name LIKE ?', "%#{params[:keyword]}%")
+        .or(Movie.where('description LIKE ?', "%#{params[:keyword]}%"))
+      )
+    end
+
+    case params[:is_showing]
+    when '1' then @movies = @movies.where(is_showing: true)
+    when '2' then @movies = @movies.where(is_showing: false)
+    end
   end
 
   # GET /movies/1 or /movies/1.json
