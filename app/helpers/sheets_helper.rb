a565132efaa624f842b44d7f503d450af0511401
tatsuya.nakai <15g079nt@gmail.com>
2024-07-05 14:38:37 +0900

    part7

--- app/helpers/sheets_helper.rb
+++ app/helpers/sheets_helper.rb
@@ -1,2 +1,9 @@
 module SheetsHelper
+  def render_rows(sheets, row)
+    sheets.select { |sheet| sheet.row == row }
+          .sort_by { |sheet| sheet.column.to_i }
+          .map { |sheet| content_tag(:td, "#{row.upcase}-#{sheet.column}", class: 'highlight') }
+          .join
+          .html_safe
+  end
 end
