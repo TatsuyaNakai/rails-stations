e6eba482d67ee45e8fc9643828ef3ae5bb3ab9e5
tatsuya.nakai <15g079nt@gmail.com>
2024-07-17 16:49:53 +0900

    fix: root画面の呼び出し方法

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    date_range = Date.today.ago(30.days)..Date.yesterday
-    @ranking_count = Ranking.preload(:movie).total_reservations_by_movie(date_range)
+    @date_range = Date.today.ago(30.days)..Date.yesterday
+    @movies = Movie.preload(:reservations)
   end
 end
